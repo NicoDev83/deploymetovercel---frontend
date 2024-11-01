4a29ee736267d95c771d25cbc0b8e9c6b481231e
Nicolas <blanc-nicolas@outlook.com>
2024-11-01 11:07:43 +0100

    frontend push 1

---
+++ script.js
@@ -0,0 +1,5 @@
+fetch('https://vercel.com/api/toolbar/link/deploymetovercel-sigma.vercel.app?via=project-dashboard-alias-list&p=1&page=/year')
+	.then(response => response.json())
+	.then(data => {
+		document.querySelector('#year').textContent = data.year
+	});
